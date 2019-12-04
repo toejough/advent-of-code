b9239485250ff15e3c81fcd015f7ba3f8ed9522d
toejough <toejough@gmail.com>
2019-12-04 03:05:07 -0500

    [REFACTOR] moved run command functions up a level to the project root

--- check.fish
+++ check.fish
@@ -12,8 +12,8 @@ and ecod gofumpt -w .
 and ecod golangci-lint run --no-config --disable-all --enable typecheck ./...
 and ecod golangci-lint run --disable typecheck ./...
 and ecod go test -failfast -timeout 1s -v ./...
-and ecod 'cd part-1 && go run .'
-and ecod 'cd part-2 && go run .'
+and ecod 'cd day-1/part-1 && go run .'
+and ecod 'cd day-1/part-2 && go run .'
 set rc $status
 echo "(last rc: $status)"
 exit $status
