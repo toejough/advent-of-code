5b7e5fb8f431eeb365cbf2b90e536e2e7a65b155
toejough <toejough@gmail.com>
2020-12-01 23:44:42 -0500

    [GREEN] test passing for 2020 day-1 part-1

---
+++ 2020/day-1/part-1/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func stripAll(items []string) (allStripped []string) {
+	for _, item := range items {
+		stripped := strings.TrimSpace(item)
+		allStripped = append(allStripped, stripped)
+	}
+	return
+}
+
+func skipEmpty(items []string) (nonEmpty []string) {
+	for _, item := range items {
+		if len(item) > 0 {
+			nonEmpty = append(nonEmpty, item)
+		}
+	}
+	return
+}
+
+func allToInts(items []string) (allInts []int, err error) {
+	for _, item := range items {
+		thisInt, err := strconv.Atoi(item)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Converting '%v' from string to int", item)
+		}
+		allInts = append(allInts, thisInt)
+	}
+	return allInts, nil
+}
+
+func solve(input string) (output string, err error) {
+	lines := strings.Split(input, "\n")
+	stripped := stripAll(lines)
+	nonEmpty := skipEmpty(stripped)
+	intItems, err := allToInts(nonEmpty)
+	if err != nil {
+		return "", errors.Wrap(err, "Converting input []string to []int")
+	}
+	for i, iItem := range intItems {
+		for _, jItem := range intItems[i:] {
+			if iItem+jItem == 2020 {
+				output = strconv.Itoa(iItem * jItem)
+				return output, nil
+			}
+		}
+	}
+	err = errors.New("Never found two values that summed to 2020")
+	return "", err
+}
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	output, err := solve(string(input))
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	fmt.Println(output)
+}
