e3ffd2fd05fe024dcf706e646f173cec23fe813b
toejough <toejough@gmail.com>
2020-12-04 02:53:04 -0500

    [GREEN] test passing for 2020 day-4 part-1

--- 2020/day-4/part-1/main_test.go
+++ 2020/day-4/part-1/main_test.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	"io/ioutil"
 	"testing"
 )
 
 func TestExample(t *testing.T) {
 	input := `
-        ..##.......
-        #...#...#..
-        .#....#..#.
-        ..#.#...#.#
-        .#...##..#.
-        ..#.##.....
-        .#.#.#....#
-        .#........#
-        #.##...#...
-        #...##....#
-        .#..#...#.#
+        ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+        byr:1937 iyr:2017 cid:147 hgt:183cm
+
+        iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+        hcl:#cfa07d byr:1929
+
+        hcl:#ae17e1 iyr:2013
+        eyr:2024
+        ecl:brn pid:760753108 byr:1931
+        hgt:179cm
+
+        hcl:#cfa07d eyr:2025 pid:166559648
+        iyr:2011 ecl:brn hgt:59in
     `
-	expectedOutput := "7"
+	expectedOutput := "2"
 
 	actualOutput, err := solve(input)
 	if err != nil {
-		t.Fatalf("Expected no errors, but got '%v'\n", err)
+		t.Fatalf("expected no error on solve, but got '%v'\n", err)
 	}
 
 	if expectedOutput != actualOutput {
@@ -35,24 +36,24 @@ func TestExample(t *testing.T) {
 	}
 }
 
-func TestSolution(t *testing.T) {
-	input, err := ioutil.ReadFile("input.txt")
-	if err != nil {
-		t.Fatalf("Error: %v", err)
-	}
+// func TestSolution(t *testing.T) {
+//    input, err := ioutil.ReadFile("input.txt")
+//    if err != nil {
+//        t.Fatalf("Error: %v", err)
+//    }
 
-	expectedOutput := "151"
+//    expectedOutput := "151"
 
-	actualOutput, err := solve(string(input))
-	if err != nil {
-		t.Fatalf("Expected no errors, but got '%v'\n", err)
-	}
+//    actualOutput, err := solve(string(input))
+//    if err != nil {
+//        t.Fatalf("Expected no errors, but got '%v'\n", err)
+//    }
 
-	if expectedOutput != actualOutput {
-		t.Fatalf(
-			"Expected output was '%v', but we got '%v' instead\n",
-			expectedOutput,
-			actualOutput,
-		)
-	}
-}
+//    if expectedOutput != actualOutput {
+//        t.Fatalf(
+//            "Expected output was '%v', but we got '%v' instead\n",
+//            expectedOutput,
+//            actualOutput,
+//        )
+//    }
+//}
