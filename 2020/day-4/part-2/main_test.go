a882da51cb2accee016c3f6281822c27e3de382f
toejough <toejough@gmail.com>
2020-12-05 14:06:31 -0500

    [GREEN] adding test for the solution now that I have the right answer for 2020 day-4 part-2

--- 2020/day-4/part-2/main_test.go
+++ 2020/day-4/part-2/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"strings"
 	"testing"
 )
@@ -131,24 +132,24 @@ func TestExampleInvalid(t *testing.T) {
 	}
 }
 
-// func TestSolution(t *testing.T) {
-// input, err := ioutil.ReadFile("input.txt")
-// if err != nil {
-// t.Fatalf("Error: %v", err)
-//}
+func TestSolution(t *testing.T) {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
 
-// expectedOutput := "206"
+	expectedOutput := "123"
 
-// actualOutput, err := solve(string(input))
-// if err != nil {
-// t.Fatalf("Expected no errors, but got '%v'\n", err)
-//}
+	actualOutput, err := solve(string(input))
+	if err != nil {
+		t.Fatalf("Expected no errors, but got '%v'\n", err)
+	}
 
-// if expectedOutput != actualOutput {
-// t.Fatalf(
-//"Expected output was '%v', but we got '%v' instead\n",
-// expectedOutput,
-// actualOutput,
-//)
-//}
-//}
+	if expectedOutput != actualOutput {
+		t.Fatalf(
+			"Expected output was '%v', but we got '%v' instead\n",
+			expectedOutput,
+			actualOutput,
+		)
+	}
+}
